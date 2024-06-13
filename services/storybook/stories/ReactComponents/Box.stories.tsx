f5ade489c49c61cb97587a4a14090df84bac0e44
uknowsj <uknowsj@gmail.com>
2024-06-13 20:32:03 +0900

    Box.stories 추가

---
+++ services/storybook/stories/ReactComponents/Box.stories.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@design-system/react-components-box";
+import "@design-system/react-components-box/style.css";
+
+export default {
+  title: "React Components/Box",
+  component: Box,
+  tags: ["autodocs"],
+};
+
+export const BoxStory = {};
